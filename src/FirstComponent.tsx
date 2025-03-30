68619f04ff5d5c16ea795a82ebb4297bfb69883a
Derin <derin.amal@tum.de>
2025-03-30 22:32:19 +0200

    add search bar

--- src/FirstComponent.tsx
+++ src/FirstComponent.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function FirstComponent() {
-	return <div>Test Component</div>;
+	const [query, setQuery] = useState('');
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setQuery(e.target.value);
+	};
+
+	const handleSearch = () => {
+		console.log("Searching for:", query);
+	};
+
+	return(
+		<div>
+			<input
+				type="text"
+				value={query}
+				onChange={handleChange}
+				placeholder="Search..."
+			/>
+			<button onClick={handleSearch}>Search</button>
+		</div>
+	);
 }
 
 export default FirstComponent;
